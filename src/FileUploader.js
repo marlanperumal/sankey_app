ca4d8eb53fb0c5f93d47956d73ece7c9d5a66abd
Marlan Perumal <marlan.perumal@gmail.com>
2018-05-27 19:26:05 +0200

    added refresh button

--- src/FileUploader.js
+++ src/FileUploader.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react'
 import Papa from 'papaparse'
 
 class FileUploader extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      file: null
+    }
+  }
   onChangeHandler(e) {
     const file = e.target.files[0]
     if (file) {
+      const { onLoad } = this.props
+      Papa.parse(file, {
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        complete: (results) => onLoad(results.data)
+      })
+      this.setState({ file })
+    }
+  }
+  onRefreshHandler(e) {
+    e.preventDefault()
+    const files = document.getElementById("input_file").files
+    if (files.length > 0) {
+      const file = files[0]
       const { onLoad } = this.props
       Papa.parse(file, {
         dynamicTyping: true,
@@ -17,10 +37,13 @@ class FileUploader extends Component {
     return (
       <form>
         <input
+          id="input_file"
           type="file"
           name="sankeyFile"
           onChange={(e) => this.onChangeHandler(e)}
+          style={{width: 500}}
         />
+        <button onClick={(e) => this.onRefreshHandler(e)}>Refresh</button>
       </form>
     )
   }
